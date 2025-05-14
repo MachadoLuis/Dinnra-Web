488fb8c9ecd384200c455e4bd42cd0c9670d7b60
Luis <machadoadriano28@gmail.com>
2025-05-14 12:27:42 -0500

    feat:addingwebelements
    -Añadiendo thymeleaf

---
+++ src/main/java/pe/dinnra_web/sistema_gestion/api/controllers/AdminWebController.java
@@ -0,0 +1,9 @@
+package pe.dinnra_web.sistema_gestion.api.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminWebController {
+}
