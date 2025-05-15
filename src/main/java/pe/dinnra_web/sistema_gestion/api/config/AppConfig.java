3090bc90b27c6447428774cf683174ba5d966048
Ekzzzzzz <mariadelosangeles505@hotmail.com>
2025-05-15 06:05:56 -0500

    quiero dormir
    dormir

--- src/main/java/pe/dinnra_web/sistema_gestion/api/config/AppConfig.java
+++ src/main/java/pe/dinnra_web/sistema_gestion/api/config/AppConfig.java
@@ -2,8 +2,10 @@ package pe.dinnra_web.sistema_gestion.api.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class AppConfig {
@@ -11,5 +13,28 @@ public class AppConfig {
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                "/public/**",
+                                "/usuario/**",
+                                "/admin/**",
+                                "/empleado/**",
+                                "/css/**",
+                                "/js/**",
+                                "/script/**",
+                                "/assets/**"
+                        ).permitAll() // Permite acceso a estas rutas sin login
+                        .anyRequest().authenticated()              // Otras requieren login
+                )
+                .formLogin(form -> form
+                        .loginPage("/public/login")                // Página de login personalizada
+                        .permitAll()
+                )
+                .logout(logout -> logout.permitAll());
+        return http.build();
     }
 }
