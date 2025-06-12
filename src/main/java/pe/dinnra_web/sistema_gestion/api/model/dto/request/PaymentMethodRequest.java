61949dbf2335dc6e67d6ca822ffa1532a4be41fc
Ekzzzzzz <U22319112@utp.edu.pe>
2025-06-11 20:42:32 -0500

    PaymentMethod
    commit

--- src/main/java/pe/dinnra_web/sistema_gestion/api/model/dto/request/PaymentMethodRequest.java
+++ src/main/java/pe/dinnra_web/sistema_gestion/api/model/dto/request/PaymentMethodRequest.java
@@ -1,4 +1,25 @@
 package pe.dinnra_web.sistema_gestion.api.model.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+
 public class PaymentMethodRequest {
+
+    @NotBlank
+    private String name;
+
+    private String description;
+
+    // Getters y setters
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
